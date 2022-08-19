73162d219969f170fe90387fef3d96c502a65cc8
Luciano Jesus Lima <luciano@doutoraovivo.com.br>
2022-08-19 15:41:39 -0300

    Set port to be defined by env

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,7 @@ import { default as swaggerUi } from 'swagger-ui-express';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { Response } from 'express';
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const bootstrap = async () => {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
